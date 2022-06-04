5d379f60dac85193a3bfeb1e18d4b891f9bb8477
Fathurrizqi300303 <106718760+Fathurrizqi300303@users.noreply.github.com>
2022-06-04 08:42:18 +0700

    Update antares.cpp
    Menambah nilai dari setiap variabel xyz pada senyawa nitrogentriflorida

--- antares.cpp
+++ antares.cpp
@@ -1,4 +1,4 @@
-%%writefile molekul-metana.cpp
+%%writefile senyawa-nitrogentriflorida.cpp
 
 #include "iostream"
 #include "cmath"
@@ -11,65 +11,23 @@ int main(){
   //deklarasi variabel
   float pi = 3.14;
 
-    // deklarasi molekul metana
-    float Cx, Cy, Cz;
-    float H1x, H1y, H1z;
-    float H2x, H2y, H2z;
-    float H3x, H3y, H3z;
-    float H4x, H4y, H4z;
+    // deklarasi senyawa nitrogentriflorida
+    float Nx, Ny, Nz;
+    float F1x, F1y, F1z;
+    float F2x, F2y, F2z;
+    float F3x, F3y, F3z;
 
-    // masukkan nilai untuk setiap variabel CH4
-    Cx = Cy = Cz = 0.0;
+    // masukkan nilai untuk setiap variabel NF3
+    Nx = Ny = Nz = 0.0;
 
-    H1x = sin(54.75*pi/180) * 1.09;
-    H1y = 0.0;
-    H1z = cos(54.75*pi/180) * 1.09;
+    F1x = sin(55.2*pi/180) * 1.373;
+    F1y = 0.0;
+    F1z = cos(55.2*pi/180) * 1.373;
 
-    H2x = - H1x;
-    H2y = H1y;
-    H2z = H1z;
+    F2x = 0.0;
+    F2y = sin(55.2*pi/180) * 1.373;
+    F2z = - cos(55.2*pi/180) * 1.373;
 
-    H3x = sin(54.75*pi/180) * 1.09;
-    H3y = 0.0;
-    H3z = cos(54.75*pi/180) * 1.09;
-
-    H4x = H3x;
-    H4y = - H3y;
-    H4z = H3z;
-    
-    //masukkan nilai tersebut ke dalam file
-    ofstream file;
-    file.open("metana.xyz");
-    file << "5\n" << endl;
-    file << setw(5) << "C" << setw(5) << " " \
-    << fixed << setprecision(5) << Cx << setw(5) << " "\
-    << fixed << setprecision(5) << Cy << setw(5) << " "\
-    << fixed << setprecision(5) << Cz << "\n";
-
-    file << setw(5) << "H" << setw(5) << " " \
-    << fixed << setprecision(5) << H1x << setw(5) << " "\
-    << fixed << setprecision(5) << H1y << setw(5) << " "\
-    << fixed << setprecision(5) << H1z << "\n";
-    
-    file << setw(5) << "H" << setw(5) << " " \
-    << fixed << setprecision(5) << H2x << setw(5) << " "\
-    << fixed << setprecision(5) << H2y << setw(5) << " "\
-    << fixed << setprecision(5) << H2z << "\n";
-
-    file << setw(5) << "H" << setw(5) << " " \
-    << fixed << setprecision(5) << H3x << setw(5) << " "\
-    << fixed << setprecision(5) << H3y << setw(5) << " "\
-    << fixed << setprecision(5) << H3z << "\n";
-
-    file << setw(5) << "H" << setw(5) << " " \
-    << fixed << setprecision(5) << H4x << setw(5) << " "\
-    << fixed << setprecision(5) << H4y << setw(5) << " "\
-    << fixed << setprecision(5) << H4z << "\n";
-
-
-    file.close();
-
-    cout << "program selesai!";
-
-    return 0;
-}
+    F3x = 0.0;
+    F3y = - F2y;
+    F3z = F2z;
