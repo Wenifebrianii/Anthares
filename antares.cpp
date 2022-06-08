1d57015e0b0ec79e79c3c1635d1fcd075981dc72
Fathurrizqi300303 <106718760+Fathurrizqi300303@users.noreply.github.com>
2022-06-09 06:20:16 +0700

    Update antares.cpp
     menambahkan panjang sel simulasi secara perhitungan, nilai dari N, Volum, Ix,Iy,Iz,lat, rcut, rcut2 dan penambahan Cout cin, If else

--- antares.cpp
+++ antares.cpp
@@ -28,42 +28,75 @@ float Ep, Ep_LJ, A12, B6;
 float rij6, rij12;
 float dx, dy, dz;
 
-int main(){
-  //deklarasi variabel
-  float pi = 3.14;
-  Int counter = 0;
+  int main(){
+    
+    rcut = 3 * sigma;
+    rcut2 = pow(rcut,2);
+    
+    cout << "program menghitung energi potensial dari nitrogentriflorida" << endl;
+    cout << "Masukkan nilai densitas dari nitrogentriflorida (eg. 1.0): ";
+    cin >> densitas;
 
+    numb_mol = densitas * (Na / Mr_Nitrogentriflorida) * cm3_to_A3;
+    cout << "Masukkan panjang sel simulasi (eg. 5): ";
+    cin >> numb_lat;
+    N = (pow(numb_lat,3)) * 3;
+    volum = (float)N / numb_mol;
+
+    // panjang sel simulasi secara perhitungan
+    lx = pow(volum,(1.0/3.0));
+    ly = lx;
+    lz = lx;
+    if(lx < (2*rcut)){
+        cout << "sorry ya nilai sel simulasi kamu terlalu besar";
+        exit(0);
+    } else{
+        cout << "panjang sel simulasi baru: " << lx << endl;
+    }
+    lat = lx / (float)numb_lat;
+    
+    //deklarasi variabel
+    float pi = 3.14;
+    
     // deklarasi senyawa nitrogentriflorida
     float Nx[125], Ny[125], Nz[125];
     float F1x[125], F1y[125], F1z[125];
     float F2x[125], F2y[125], F2z[125];
     float F3x[125], F3y[125], F3z[125];
+    
+    /*      F
+            |
+            N
+           / \
+          F   F
+     molekul Nitrogentriflorida itu bentuk rigid
+    */
 
     // masukkan nilai untuk setiap variabel NF3
     float rB_Nx = 0.0;
     float rB_Ny = 0.0;
     float rB_Nz = 0.0;
 
-    float rB_F1x = sin(55.2*pi/180) * 1.373;
+    float rB_F1x = sin(54.7355*pi/180) * 1.470;
     float rB_F1y = 0.0;
-    float rB_F1z = cos(55.2*pi/180) * 1.373;
+    float rB_F1z = cos(54.7355*pi/180) * 1.470;
 
     float rB_F2x = 0.0;
-    float rB_F2y = sin(55.2*pi/180) * 1.373;
-    float rB_F2z = - cos(55.2*pi/180) * 1.373;
+    float rB_F2y = sin(54.7355*pi/180) * 1.470;
+    float rB_F2z = - cos(54.7355*pi/180) * 1.470;
 
-    float rB_F3x = 0.0;
+    float rB_F3x = rB_F2x;
     float rB_F3y = - rB_F2y;
     float rB_F3z = rB_F2z;
     
-    // iterasi untuk setiap senyawa nitrogentriflorida
-    for(int i = 0; i < 5; i++){
-    for(int j = 0; j < 5; j++){
-    for(int k = 0; k < 5; k++){
+    int counter = 0;
+        for(int i = 0; i < numb_lat; i++){ //x
+           for(int j = 0; j < numb_lat; j++){ //y
+               for(int k = 0; k < numb_lat; k++){ //z 
  
-    Nx[counter] = rB_Nx + (i*3.01);
-    Ny[counter] = rB_Ny + (j*3.01);
-    Nz[counter] = rB_Nz + (k*3.01);
+    Nx[counter] = rB_Nx + (i*lat);
+    Ny[counter] = rB_Ny + (j*lat);
+    Nz[counter] = rB_Nz + (k*lat);
     
     F1x[counter] = Nx[counter] + rB_F1x;
     F1y[counter] = Ny[counter] + rB_F1y;
@@ -73,46 +106,10 @@ int main(){
     F2y[counter] = Ny[counter] + rB_F2y;
     F2z[counter] = Nz[counter] + rB_F2z;
 
-F3x[counter] = Nx[counter] + rB_F3x;
+    F3x[counter] = Nx[counter] + rB_F3x;
     F3y[counter] = Ny[counter] + rB_F3y;
     F3z[counter] = Nz[counter] + rB_F3z;
     counter += 1;
     }
     }
     }
-
-    // hasil iterasi dimasukkan ke dalam file xyz
-    ofstream file;
-    file.open("nitrogentriflorida-ruah.xyz");
-    int N = 125*4;
-    file << N << "\n" << endl;
-
-    // iterasi dalam output array
-    for (int m = 0; m < 125; m++){
-    file << setw(4) << "N" << setw(4) << " "\
-         << fixed << setprecision(4) << Nx[m] << setw(4) << " "\
-         << fixed << setprecision(4) << Ny[m] << setw(4) << " "\
-         << fixed << setprecision(4) << Nz[m] << "\n";
-
-    file << setw(4) << "F" << setw(4) << " "\
-         << fixed << setprecision(4) << F1x[m] << setw(4) << " "\
-         << fixed << setprecision(4) << F1y[m] << setw(4) << " "\
-         << fixed << setprecision(4) << F1z[m] << "\n";
-
-    file << setw(4) << "F" << setw(4) << " "\
-         << fixed << setprecision(4) << F2x[m] << setw(4) << " "\
-         << fixed << setprecision(4) << F2y[m] << setw(4) << " "\
-         << fixed << setprecision(4) << F2z[m] << "\n";
- 
-    file << setw(4) << "F" << setw(4) << " "\
-         << fixed << setprecision(4) << F3x[m] << setw(4) << " "\
-         << fixed << setprecision(4) << F3y[m] << setw(4) << " "\
-         << fixed << setprecision(4) << F3z[m] << "\n";
-    }
-
-    file.close();
-
-    cout << "program selesai!";
-
-    return 0;
-}
