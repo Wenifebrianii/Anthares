f5827fb4fadf8ab3f0deec5bb43fd8542ea11f3b
Fathurrizqi300303 <106718760+Fathurrizqi300303@users.noreply.github.com>
2022-06-08 18:01:36 +0700

    Update antares.cpp
    Menambahkan float dari densitas, cm3_to_A3, numb_mol, Na, Mr_metana, volum, lx, ly, lz, lat, sigma, epsilon, rij, rcut, rcut2, Ep, Ep_LJ, A12, B6, rij6, rij12, dan dx, dy, dz. Lalu menambahkan int numb_lat dan int N.

--- antares.cpp
+++ antares.cpp
@@ -7,6 +7,27 @@
 
 using namespace std;
 
+float densitas;         /*densitas ini memiliki satuan g cm^-3
+                          dan ini harus diinput dari user */
+// converter dari nilai cm^-3 ke A^3
+float cm3_to_A3 = 1.0E-24;
+float numb_mol;
+float Na = 6.022E23;                        // bilangan avogadro
+float Mr_nitrogentriflorida = 71.0;         // massa relatif dari nitrogentriflorida
+int numb_lat;                               // panjang sel simulasi
+int N;                                      // jumlah molekul
+float volum;                                // volume dari molekul nitrogentriflorida
+float lx, ly, lz;                           // panjang sel simulasi terhitung 
+float lat;                                  // jarak antar molekul nitrogentriflorida
+
+// variabel perhitungan energi potensial
+float sigma = 2.725;                        // satuan (A)
+float epsilon = 3.725;                      // satuan (kj/mol)
+float rij, rcut, rcut2;
+float Ep, Ep_LJ, A12, B6;
+float rij6, rij12;
+float dx, dy, dz;
+
 int main(){
   //deklarasi variabel
   float pi = 3.14;
